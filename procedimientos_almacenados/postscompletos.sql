4a8808b8c7ec6df4292057099364f54037d15915
Julian <sodojulian@gmail.com>
2019-02-12 21:25:29 -0300

    SP actualizado :

--- procedimientos_almacenados/postscompletos.sql
+++ procedimientos_almacenados/postscompletos.sql
@@ -1,23 +1,20 @@
-DELIMITER $$
+
 CREATE PROCEDURE `postscompletos`(IN `puserID` INT)
 BEGIN
+CREATE TEMPORARY TABLE tmp_POSTS as (SELECT posts.* from posts  WHERE posts.userID = puserID AND posts.esREPOST = 0);
 
-CREATE TEMPORARY TABLE POSTS as (SELECT posts.* from posts WHERE posts.userID = puserID);
+CREATE TEMPORARY TABLE tmp_REPOSTS as (SELECT posts.* from posts  WHERE posts.userID = puserID AND posts.esREPOST = 1);
 
-CREATE TEMPORARY TABLE REPOSTS as (SELECT posts.* from posts WHERE posts.userID = puserID and posts.esREPOST = 1);
+CREATE TEMPORARY TABLE tmp_AMBOS as (
+SELECT tmp_POSTS.id, tmp_POSTS.userID, tmp_POSTS.texto, tmp_POSTS.shares, tmp_POSTS.likes, tmp_POSTS.fecha ,tmp_POSTS.esREPOST, tmp_POSTS.idREPOST, usuarios.usuario, usuarios.nombre, imagenes.url as perfil_img FROM tmp_POSTS  INNER JOIN usuarios ON usuarios.id = tmp_POSTS.userID INNER JOIN imagenes ON imagenes.userID = tmp_POSTS.userID);
 
-CREATE TEMPORARY TABLE AMBOS AS
-(SELECT POSTS.*,  imagenes.url FROM POSTS inner join imagenes on imagenes.userID = POSTS.userID WHERE POSTS.esREPOST = 0);
+INSERT INTO tmp_AMBOS(
+SELECT tmp_REPOSTS.id, tmp_REPOSTS.userID, posts.texto, posts.shares, posts.likes, tmp_REPOSTS.fecha,1,0,usuarios.usuario, usuarios.nombre, imagenes.url as perfil_img FROM tmp_REPOSTS INNER JOIN posts ON posts.id = tmp_REPOSTS.idREPOST INNER JOIN usuarios ON usuarios.id = posts.userID INNER JOIN imagenes ON imagenes.userID = posts.userID);
+
+SELECT * FROM tmp_AMBOS order by fecha desc;
 
 
-INSERT INTO AMBOS     
-SELECT REPOSTS.id, REPOSTS.userID, REPOSTS.texto, REPOSTS.shares, REPOSTS.likes, posts.fecha, posts.esREPOST, posts.idREPOST, imagenes.url FROM REPOSTS inner join imagenes on imagenes.userID = REPOSTS.userID INNER JOIN posts ON posts.id = REPOSTS.id;
-
-
-SELECT AMBOS.id, AMBOS.userID, AMBOS.texto, AMBOS.shares, AMBOS.likes, AMBOS.fecha, AMBOS.esREPOST, AMBOS.idREPOST, usuarios.usuario, usuarios.nombre, AMBOS.url as perfil_img FROM AMBOS INNER JOIN usuarios ON usuarios.id = AMBOS.userID ORDER BY fecha desc;
-
-DROP TABLE POSTS;
-DROP TABLE REPOSTS;
-DROP TABLE AMBOS;
-END$$
-DELIMITER ;
+DROP TABLE tmp_POSTS;
+DROP TABLE tmp_REPOSTS;
+DROP TABLE tmp_AMBOS;
+END
