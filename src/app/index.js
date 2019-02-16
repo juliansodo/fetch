696eab8b52f17002561ac8a00c7566fa9db8e509
Juan <juan.colombo95@gmail.com>
2019-02-16 13:54:42 -0300

    Vue-Registro
    Pasar el registro a vue -validacion con vue

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import indexSync from './componentes/indexSync.vue';
 import indexNoSync from './componentes/indexNoSync.vue';
+import registro from './componentes/registro.vue';
+
 new Vue({
     render: h => h(indexSync)
 }).$mount("#indexSync");
@@ -10,4 +12,10 @@ new Vue({
 
     render: h => h(indexNoSync)
 }).$mount("#indexNoSync");
+
+new Vue({
+
+    render: h => h(registro)
+}).$mount("#registro");
+
 
