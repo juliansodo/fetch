fcfd927bb106fd172643c97d6c2b091646b8de57
Juan <juan.colombo95@gmail.com>
2019-02-17 20:37:17 -0300

    UploadIMG
    instalar muter : npm i muter
    Implemetacion de muter

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const session = require("cookie-session");
 const uploadfiles = require("express-fileupload");
 const busboy = require("connect-busboy");
 const timeago = require("timeago.js");
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+    destination:path.join(__dirname,"public/img"),
+    filename: (req,file,cb) =>{
+       cb(null,file.originalname) 
+    }
+})
 
 //Declaraciones
 app.set("puerto", process.env.PORT || 3000);
@@ -20,6 +28,14 @@ app.locals.formatearHora = function(hora)
 };
 
 //MIDDLEWARES - Funciones que deben ejecutarse antes de cargarse las rutas.
+
+//Multer -- Subir imagenes
+app.use(multer({
+    storage,
+    dest: path.join(__dirname,"public/img"),
+    limits : {fileSize:2000000}
+}).single('image'))
+
 //le especfico que las vistas estarán en la carpeta /vistas
 app.set('views', path.join(__dirname, '/vistas'));
 //le especifico que el motor de plantillas será ejs.
